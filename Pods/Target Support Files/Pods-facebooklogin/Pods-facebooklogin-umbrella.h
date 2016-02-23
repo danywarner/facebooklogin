04e595fa05cbb571490f2d11f1750b0c5280e06a
Daniel Warner <danywarner@gmail.com>
2016-02-23 16:43:17 -0500

    UI + facebook login working (returning token)

---
+++ Pods/Target Support Files/Pods-facebooklogin/Pods-facebooklogin-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_facebookloginVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_facebookloginVersionString[];
+
---
+++ facebooklogin/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  facebooklogin
+//
+//  Created by Daniel Warner on 2/23/16.
+//  Copyright © 2016 Daniel Warner. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0
